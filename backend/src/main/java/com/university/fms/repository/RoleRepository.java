a6b470b1d8f0b05d4aacc034410e06b583bd812a
harshadgawade <harshadgawade40@gmail.com>
2026-09-05 23:54:07 +0530

    Add JPA repository for faculty role lookup

---
+++ backend/src/main/java/com/university/fms/repository/RoleRepository.java
@@ -0,0 +1,6 @@
+package com.university.fms.repository;
+
+import com.university.fms.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {}
